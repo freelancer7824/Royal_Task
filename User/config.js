c7bb736b490d9696dcf020691a313a579e6f8db1
SHOPON HASAN PRIYO <freelancer7824@gmail.com>
2026-06-19 12:23:16 +0600

    Add files via upload

---
+++ User/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjoRYAXQa4J43L4oLmHmUoK7ZqMKCdrxI",
+  authDomain: "programme-7eb45.firebaseapp.com",
+  databaseURL: "https://programme-7eb45-default-rtdb.firebaseio.com",
+  projectId: "programme-7eb45",
+  storageBucket: "programme-7eb45.firebasestorage.app",
+  messagingSenderId: "431090813085",
+  appId: "1:431090813085:web:496fc0d8bb5e57af53ec74",
+  measurementId: "G-FE49VB7F1B"
+};
