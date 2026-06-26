15d1a1db6ca13c3c1d70b8e389ff2cd29a4c2714
SHOPON HASAN PRIYO <freelancer7824@gmail.com>
2026-06-26 15:11:13 +0600

    Add files via upload

---
+++ User/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnPqglycsnoV-iCjTNe4K-hmjTIxHhHwk",
+  authDomain: "royal-task-8be99.firebaseapp.com",
+  projectId: "royal-task-8be99",
+  storageBucket: "royal-task-8be99.firebasestorage.app",
+  messagingSenderId: "206881011103",
+  appId: "1:206881011103:web:e7e86157cdcd1d9a2039aa",
+  measurementId: "G-4RLCKGHX26"
+};
